07ad56843d9789191cbb6159786f58d5465e1782
Brian Purnell <purnellbp@gmail.com>
2020-06-25 17:29:58 -0500

    added styling for song list

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, {Component} from "react";
 import {connect} from 'react-redux';
+import {selectSong} from "../actions";
 
-class SongList extends React.Component {
+class SongList extends Component {
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary">
+                            Select
+                        </button>
+                    </div>
+
+                    <div className="content">{song.title}</div>
+                </div>
+            );
+        });
+    }
+
     render() {
         console.log(this.props);
-        return (
-            <div>SongList</div>
-        );
+        return <div className="ui divided list">{this.renderList()}</div>;
     }
 }
 
@@ -14,5 +29,5 @@ const mapStateToProps = (state) => {
     return {songs: state.songs};
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {selectSong})(SongList);
 
